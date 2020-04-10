982e74333fb5b114034e14dd41c0fefc23496a64
Alex Buzlov <alex@cloudcoreo.com>
2020-04-10 15:48:40 +0600

    body-parser for req.body

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const TicketsController = require('./controllers/TicketsController');
 const logger = require('./services/LoggerService');
 const mongoose = require('./DB/MongooseModule');
 
 const bootstrap = async () => {
   const app = express();
-
+  app.use(bodyparser.urlencoded({ extended: true }));
+  app.use(bodyparser.json());
   await mongoose.connect();
 
   app.use('/tickets', TicketsController);
